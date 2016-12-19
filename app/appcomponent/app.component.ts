8dda8e58f749e6d9cac341de6ed229b8a9dd4ab7
Avani Jhanwar <avanijhawar20@gmail.com>
2016-12-19 17:51:21 +0530

    edited files to add styling

--- app/appcomponent/app.component.ts
+++ app/appcomponent/app.component.ts
@@ -1,11 +1,13 @@
 
-
 import {Component} from '@angular/core';
 import {BookService}  from '../common/books.service';
+
+
 
 @Component({
     selector: 'app',
     templateUrl: 'app/appcomponent/app.component.html',
+    styleUrls: ['app/common/style.css'],
       providers:[BookService]  
  })
 
