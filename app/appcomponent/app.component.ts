779480b10fb261c96ac5fc4b1cc45e3456afcd95
Avani Jhanwar <avanijhawar20@gmail.com>
2016-12-20 17:48:15 +0530

    Changes in Font styles

--- app/appcomponent/app.component.ts
+++ app/appcomponent/app.component.ts
@@ -3,11 +3,10 @@ import {Component} from '@angular/core';
 import {BookService}  from '../common/books.service';
 
 
-
 @Component({
     selector: 'app',
     templateUrl: 'app/appcomponent/app.component.html',
-    styleUrls: ['app/common/style.css'],
+    styleUrls: ['app/common/style.css','app/appcomponent/app.component.css'],
       providers:[BookService]  
  })
 
